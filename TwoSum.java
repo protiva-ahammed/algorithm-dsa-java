273e98fe4a19a1630221b104209eff4cbe8eee63
ahammed-protiva <protivaahammed7137@gmail.com>
2025-08-05 21:17:47 +0900

    two sum in bruteforce

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class TwoSum {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        for (int i = 0 ;i <n;i++){
+            for(int j = i+1;j< n;j++)
+                if((target-numbers[i]) == numbers[j])
+                    return new int[]{i+1,j+1};
+        }
+        return new int[]{1,2};
+    }
     public static void main(String[] args) {
-        
+        int[] arr  = {1,2,3,4};
+        System.out.println(Arrays.toString(twoSum(arr , 6)));
     }
 }
