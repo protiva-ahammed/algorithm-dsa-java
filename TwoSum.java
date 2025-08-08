325f244313b048588099312a8d0df99c229c0fe4
ahammed-protiva <protivaahammed7137@gmail.com>
2025-08-09 01:48:52 +0900

    two pointer implementation

--- TwoSum.java
+++ TwoSum.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class TwoSum {
 
-    public static int[] twoSum(int[] numbers, int target) {
+    public static int[] twoSumBruteForce(int[] numbers, int target) {
         int n = numbers.length;
         for (int i = 0 ;i <n;i++){
             for(int j = i+1;j< n;j++)
@@ -11,8 +11,24 @@ public class TwoSum {
         }
         return new int[]{1,2};
     }
+
+    public static int[] twoSumTwoPointer(int[] numbers, int target) {
+        int end = numbers.length -1;
+        int first = 0;
+        while (first<end){
+            if((numbers[first]+ numbers[end]) >target)
+                end -=1;
+            else if(numbers[first]+ numbers[end]<target)
+                first +=1;
+            else
+                return new int[]{first +1,end +1};
+        }
+        return new int[]{1,2};
+    }
     public static void main(String[] args) {
         int[] arr  = {1,2,3,4};
-        System.out.println(Arrays.toString(twoSum(arr , 6)));
+        System.out.println(Arrays.toString(twoSumTwoPointer(arr , 6)));
+        System.out.println(Arrays.toString(twoSumBruteForce(arr , 6)));
+
     }
 }
